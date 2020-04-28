3f972e197b96a240305dab7ec16ecc0cba117c54
Jung Lee <lee.jung.s95@gmail.com>
2020-04-28 03:16:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,7 @@
 # Add  code here!
 def prime?(num)
   (2..(num - 1)).each do |n|
-    if num % n == 0 || num < 0
-      return false
-    else
-      true
+    return false if num % n == 0 || num < 0
     end
   end
 end
